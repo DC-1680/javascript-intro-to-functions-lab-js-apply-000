5fd1db284624e2c61b77534f126ea0bb2e6d8772
Daryl Concha <concha.daryl@gmail.com>
2018-04-02 17:00:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too.";
   } else if (string.toUpperCase() === uppercase){
-    return "I can\'t hear you!";
-  } else {
     return "YES INDEED!";
+  } else {
+    return "I can\'t hear you!";
   }
 }
