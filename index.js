a251fe638b35d94c204be0da33f1d2020161ca32
Daryl Concha <concha.daryl@gmail.com>
2018-04-02 16:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 function shout(string){
     return string.toUpperCase(); 
 
+};
+
+function whisper(string){
+  
+};
+
+function logShout(string){
+  
+};
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+};
+/*
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma."){
+    return "I love you, too.";
+  } else if (){
+    
+  } else if (){
+    
+  }
+  */
+  
 }
